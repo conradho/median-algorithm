07b5d2d93311070c3331b9037371728855ae5b10
conrad <conrad@pythonanywhere.com>
2016-09-02 15:35:24 +0100

    add a hypothesis test for python quickselect

--- test_median.py
+++ test_median.py
@@ -1,5 +1,14 @@
-from python_median import get_kth_element
+from hypothesis import given
+from hypothesis.strategies import lists, integers
 
+from python_median import get_kth_element
 
 def test_median_works_for_single_item():
     assert get_kth_element([1], 0) ==  1
+
+@given(lists(integers(), min_size=1))
+def test_median_works(nonempty_list_of_integers):
+    sorted_list = sorted(nonempty_list_of_integers)
+    for kth, element in enumerate(sorted_list):
+        kth_element = get_kth_element(nonempty_list_of_integers, kth)
+        assert  kth_element == element, 'The element returned at index {} is wrong'.format(kth)

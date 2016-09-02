c091a82ca3641aba569228fc8d00957193c7d90b
conrad <conrad@pythonanywhere.com>
2016-09-02 20:14:59 +0100

    rewrite partition function to take indexes and left/right bounds instead of value

--- python_median.py
+++ python_median.py
@@ -1,24 +1,27 @@
 
-def partition(unordered_list, pivot_value):
-    # this needs to be linear time
-    # currently O(2n) if all elements in the right
+def partition(unordered_list, pivot_index, left_bound, right_bound):
+    # make sure we have pivot inside of left/right bound
+    assert 0 <= left_bound <= pivot_index < right_bound <= len(unordered_list)
 
     left = []
+    pivot = unordered_list[pivot_index]
     right = []
-    found_pivot = False
 
-    for element in unordered_list:
-        if element < pivot_value:
+    for i, element in enumerate(unordered_list):
+        if i < left_bound:
             left.append(element)
-        elif not found_pivot and element == pivot_value:
-            found_pivot = True
+        elif i >= right_bound:
+            right.append(element)
+        elif i == pivot_index:
+            continue
+        elif element < pivot:
+            left.append(element)
         else:
             right.append(element)
 
-    # yay len() is O(1)
     pivot_index = len(left)
 
-    return (left + [pivot_value] + right, pivot_index)
+    return (left + [pivot] + right, pivot_index)
 
 def get_kth_element(unordered_list, kth, left_index, right_index):
     if left_index == right_index:
--- test_partition.py
+++ test_partition.py
@@ -1,19 +1,54 @@
+from collections import namedtuple
 from hypothesis import given
-from hypothesis.strategies import lists, integers, choices
+from hypothesis.strategies import lists, integers, composite
 
 from python_median import partition
 
 
-@given(lists(integers(), min_size=1), choices())
-def test_partition_splits_list_by_comparing_to_pivot(unordered_list, choice):
-    pivot = choice(unordered_list)
+@composite
+def partition_params(draw):
+    # use named tuples so errors are more readable
+    PartitionParams = namedtuple('PartitionParams', 'xs index left right')
+    xs = draw(lists(integers(), min_size=1))
+    max_index = len(xs) - 1
+    left_bound = draw(integers(min_value=0, max_value=max_index))
+    pivot_index = draw(integers(min_value=left_bound, max_value=max_index))
+    right_bound = draw(integers(min_value=pivot_index + 1, max_value=len(xs)))
+    return PartitionParams(
+        xs=xs, index=pivot_index, left=left_bound, right=right_bound
+    )
 
-    partitioned_list, pivot_index = partition(unordered_list, pivot)
+@given(partition_params())
+def test_does_not_change_elements_outside_of_bounds(hypothesis_test_case):
+    unordered_list, pivot_index, left_bound, right_bound = hypothesis_test_case
+    partitioned_list, _ = partition(unordered_list, pivot_index, left_bound, right_bound)
+
+    assert partitioned_list[:left_bound] == unordered_list[:left_bound]
+    assert partitioned_list[right_bound:] == unordered_list[right_bound:]
+
+@given(partition_params())
+def test_returns_correct_new_pivot_position(hypothesis_test_case):
+    unordered_list, pivot_index, left_bound, right_bound = hypothesis_test_case
+    partitioned_list, new_pivot_index = partition(unordered_list.copy(), pivot_index, left_bound, right_bound)
+    pivot_value = unordered_list[pivot_index]
+
+    assert partitioned_list[new_pivot_index] == pivot_value
+
+@given(partition_params())
+def test_is_not_missing_elements(hypothesis_test_case):
+    unordered_list, pivot_index, left_bound, right_bound = hypothesis_test_case
+    partitioned_list, _ = partition(unordered_list.copy(), pivot_index, left_bound, right_bound)
 
     assert len(partitioned_list) == len(unordered_list)
-    assert partitioned_list[pivot_index] == pivot
 
-    for element in partitioned_list[:pivot_index]:
-        assert element < pivot, 'found elements bigger than pivot before pivot'
-    for element in partitioned_list[pivot_index:]:
-        assert element >= pivot, 'found elements smaller than pivot after pivot'
+
+@given(partition_params())
+def test_splits_list_by_comparing_to_pivot(hypothesis_test_case):
+    unordered_list, pivot_index, left_bound, right_bound = hypothesis_test_case
+    partitioned_list, new_pivot_index = partition(unordered_list.copy(), pivot_index, left_bound, right_bound)
+    pivot_value = unordered_list[pivot_index]
+
+    for element in partitioned_list[left_bound:new_pivot_index]:
+        assert element < pivot_value, 'found elements bigger than pivot before pivot'
+    for element in partitioned_list[new_pivot_index:right_bound]:
+        assert element >= pivot_value, 'found elements smaller than pivot after pivot'
